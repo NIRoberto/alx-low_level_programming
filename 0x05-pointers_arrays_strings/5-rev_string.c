452a24e766581725a95f7c10b7072ef759e13017
niroberto <robertwilly668@gmail.com>
2023-04-08 08:56:21 -0700

    initial commit

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -8,6 +8,15 @@
  
 void rev_string(char *s)
 {
-	strrev(s);
+	int len = strlen(s);
+	char *p1 = s;
+	char *p2 = s + len - 1;
+
+	while (p1 < p2)
+	{
+		char t = *p1;
+		*p1++ = *p2;
+		*p2 = t;
+	}
 }
 
