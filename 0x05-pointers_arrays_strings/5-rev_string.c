b6957676e64f63d9be3ae062f91750dd59da5934
niroberto <robertwilly668@gmail.com>
2023-04-08 08:43:13 -0700

    initial commit

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,13 @@
+#include<string.h>
+#include"main.h"
+
+/**
+ * rev_string - function to reverse a string
+ * @s: inputed string to be reversed 
+ */
+ 
+void rev_string(char *s)
+{
+	strrev(s);
+}
+
