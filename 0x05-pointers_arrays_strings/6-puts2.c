fed4f328d9ceeda8de9c2048f86251992ef96bbb
niroberto <robertwilly668@gmail.com>
2023-04-08 10:27:44 -0700

    initial commit

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,7 +1,7 @@
 #include"main.h"
 
 /**
- * puts2 - function to print even numbr strings 
+ * puts2 - function to print even numbr strings
  * @str: string to be printed
  */
 void puts2(char *str)
@@ -10,7 +10,7 @@ void puts2(char *str)
 
 	for (i = 0; str[i] != '\0'; i++)
 	{
-		if ((i %= 2) == 0)
+		if ((i % 2) == 0)
 		{
 			putchar(str[i]);
 		}
