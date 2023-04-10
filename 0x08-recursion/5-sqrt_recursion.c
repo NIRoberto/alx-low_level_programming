29213aa10bd90d9b751e6ded689a9595b7d39389
niroberto <robertwilly668@gmail.com>
2023-04-10 12:22:00 -0700

    recusion

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,16 @@
+#include<math.h>
+#include"main.h"
+
+/**
+ * _sqrt_recursion - function to compute the square root of a number
+ * @n: inputednumber
+ * Return: square of a number
+ */
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	return sqrt(x);
+}
